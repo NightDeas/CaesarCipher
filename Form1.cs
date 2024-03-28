823434952e2432233f1132609491a1567a9a283b
1 <1@DESKTOP-JJSJSDV>
2024-03-28 19:29:01 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    public partial class Form1 : Form
+    {
+        bool reverse = false;
+        enum Language
+        {
+            rus,
+            eng
+        }
+        static readonly char[] EngChar = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' }; // 26 букв
+        static readonly char[] RusChar = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };// 33 букв
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox_Text.Text;
+            int sdvig = (int)numericUpDown1.Value;
+            sdvig = reverse ? -sdvig : sdvig;
+            string result = "";
+            char[] chars = text.ToCharArray(); // массив символов из text
+
+            if (FindNumber(chars) == false) // проверка массива на наличие цифр
+            {
+                label_Error.Visible = false;
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    bool isLower = char.IsLower(chars[i]);
+                    char symbol = char.ToLower(chars[i]);
+                    char symbolResult = FindSymbol(symbol, comboBox1.SelectedIndex == 0 ? Language.eng : Language.rus, sdvig);
+                    symbolResult = isLower ? symbolResult : char.ToUpper(symbolResult);
+                    result += symbolResult;
+                }
+            }
+            else
+                label_Error.Visible = true;
+            textBox_Result.Text = result;
+        }
+
+        static bool FindNumber(char[] chars)
+        {
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] >= '0' && chars[i] <= '9')
+                    return true;
+            }
+            return false;
+        }
+
+        static char FindSymbol(char a, Language language, int sdvig)
+        {
+            int? index = null;
+            char[] langArr;
+            switch (language)
+            {
+                case Language.rus:
+                    langArr = RusChar;
+                    break;
+                case Language.eng:
+                    langArr = EngChar;
+                    break;
+                default:
+                    langArr = RusChar;
+                    break;
+            }
+
+
+            for (int i = 0; i < langArr.Length; i++)
+            {
+                if (langArr[i] == a)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index != null)
+            {
+                int indexFind = (int)index + sdvig;
+                while (indexFind < 0)
+                {
+                    indexFind = langArr.Length + sdvig + (int)index;
+
+                }
+                while (indexFind > langArr.Length - 1)
+                {
+                    indexFind -= langArr.Length;
+                }
+                return (char)langArr.GetValue(indexFind);
+            }
+            return a;
+        }
+    
+        private void CbOperationSelect(object sender, EventArgs e)
+        {
+            if ((sender as ComboBox).SelectedIndex == 0)
+            {
+                button_Start.Text = "Зашифровать";
+                reverse = false;
+            }
+            else
+            {
+                button_Start.Text = "Расшифровать";
+                reverse = true;
+            }
+        }
+
+        private void CbLanguageSelect(object sender, EventArgs e)
+        {
+            if ((sender as ComboBox).SelectedIndex == 0)
+                numericUpDown1.Maximum = EngChar.Length;
+            else
+                numericUpDown1.Maximum = RusChar.Length;
+            numericUpDown1.Enabled = true;
+        }
+    }
+}
---
+++ Form1.designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ConsoleApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Text = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.label_Error = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите текст";
+            // 
+            // textBox_Text
+            // 
+            this.textBox_Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox_Text.Location = new System.Drawing.Point(162, 10);
+            this.textBox_Text.Name = "textBox_Text";
+            this.textBox_Text.Size = new System.Drawing.Size(353, 29);
+            this.textBox_Text.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(13, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Шифр Цезаря:";
+            // 
+            // textBox_Result
+            // 
+            this.textBox_Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox_Result.Location = new System.Drawing.Point(162, 202);
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.ReadOnly = true;
+            this.textBox_Result.Size = new System.Drawing.Size(353, 29);
+            this.textBox_Result.TabIndex = 3;
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(521, 205);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(108, 28);
+            this.button_Start.TabIndex = 4;
+            this.button_Start.Text = "Зашифровать";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_Error
+            // 
+            this.label_Error.AutoSize = true;
+            this.label_Error.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label_Error.ForeColor = System.Drawing.Color.Red;
+            this.label_Error.Location = new System.Drawing.Point(17, 258);
+            this.label_Error.Name = "label_Error";
+            this.label_Error.Size = new System.Drawing.Size(313, 27);
+            this.label_Error.TabIndex = 5;
+            this.label_Error.Text = "В ПОЛЕ ПРИСУСТВУЮТ ЦИФРЫ";
+            this.label_Error.UseCompatibleTextRendering = true;
+            this.label_Error.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label4.Location = new System.Drawing.Point(12, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(432, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "На сколько требуется выполнить сдвиг(Ключ):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label3.Location = new System.Drawing.Point(13, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Выберите язык";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Английский язык",
+            "Русский язык"});
+            this.comboBox1.Location = new System.Drawing.Point(162, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(353, 32);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.CbLanguageSelect);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Enabled = false;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.numericUpDown1.Location = new System.Drawing.Point(450, 146);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 29);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label5.Location = new System.Drawing.Point(13, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(200, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Выберите операцию:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Зашифровать",
+            "Расшифровать"});
+            this.comboBox2.Location = new System.Drawing.Point(217, 101);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(298, 32);
+            this.comboBox2.TabIndex = 11;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.CbOperationSelect);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 322);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label_Error);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.textBox_Result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_Text);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(672, 361);
+            this.MinimumSize = new System.Drawing.Size(672, 361);
+            this.Name = "Form1";
+            this.Text = "Шифр Цезаря";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Text;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Result;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Label label_Error;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
